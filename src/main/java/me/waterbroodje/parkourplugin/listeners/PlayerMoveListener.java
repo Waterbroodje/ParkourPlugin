2c962fb3acc3e78548cc5b82c74c3548aec87d82
Waterbroodje <wortelschaaf.info@gmail.com>
2022-01-08 15:05:59 +0100

    Added code, with todo comments.

--- src/main/java/me/waterbroodje/parkourplugin/listeners/PlayerMoveListener.java
+++ src/main/java/me/waterbroodje/parkourplugin/listeners/PlayerMoveListener.java
@@ -1,5 +1,7 @@
 package me.waterbroodje.parkourplugin.listeners;
 
+import me.waterbroodje.parkourplugin.Main;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -8,6 +10,11 @@ public class PlayerMoveListener implements Listener {
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent e) {
-
+        Player player = e.getPlayer();
+        if (Main.getWorldGuardHelper().isInRegion(e.getTo(), "parkour_region")) {
+            //todo: add player to scoreboard
+        } else if (Main.getWorldGuardHelper().isInRegion(e.getFrom(), "parkour_region")) {
+            //todo: remove player from scoreboard
+        }
     }
 }
