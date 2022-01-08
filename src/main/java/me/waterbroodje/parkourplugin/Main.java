198a5f905a5d59c2ecb4354e0df3ae0f24c38b2f
Waterbroodje <wortelschaaf.info@gmail.com>
2022-01-08 14:03:47 +0100

    Added instance

--- src/main/java/me/waterbroodje/parkourplugin/Main.java
+++ src/main/java/me/waterbroodje/parkourplugin/Main.java
@@ -1,16 +1,31 @@
 package me.waterbroodje.parkourplugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
 
 public final class Main extends JavaPlugin {
 
+    public static Main instance;
+
     @Override
     public void onEnable() {
-
+        instance = this;
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
+
+    public void registerEvents(Listener... listeners) {
+        Arrays.asList(listeners).forEach(listener ->
+                Bukkit.getPluginManager().registerEvents(listener, this));
     }
 }
