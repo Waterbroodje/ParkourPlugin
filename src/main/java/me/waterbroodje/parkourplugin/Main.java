8b49754e883014a44e067ef75033bcff648b6c99
Waterbroodje <wortelschaaf.info@gmail.com>
2022-01-08 15:05:32 +0100

    Edited some things

--- src/main/java/me/waterbroodje/parkourplugin/Main.java
+++ src/main/java/me/waterbroodje/parkourplugin/Main.java
@@ -1,26 +1,22 @@
 package me.waterbroodje.parkourplugin;
 
-import com.sk89q.worldguard.WorldGuard;
-import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-import com.sk89q.worldguard.protection.regions.RegionContainer;
+import me.waterbroodje.parkourplugin.domain.WorldGuardHelper;
 import me.waterbroodje.parkourplugin.listeners.PlayerMoveListener;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
 
 public final class Main extends JavaPlugin {
 
     private static Main instance;
-    public List<UUID> playerInRegion = new ArrayList<>();
+    private static WorldGuardHelper worldGuardHelper;
 
     @Override
     public void onEnable() {
         instance = this;
+        worldGuardHelper = new WorldGuardHelper();
 
         registerEvents(
                 new PlayerMoveListener()
@@ -29,6 +25,10 @@ public final class Main extends JavaPlugin {
 
     public static Main getInstance() {
         return instance;
+    }
+
+    public static WorldGuardHelper getWorldGuardHelper() {
+        return worldGuardHelper;
     }
 
     public void registerEvents(Listener... listeners) {
