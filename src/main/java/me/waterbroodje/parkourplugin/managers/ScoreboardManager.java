4a54d5ac482d302304a73eaf9921d215661e8d74
Waterbroodje <wortelschaaf.info@gmail.com>
2022-01-11 15:01:28 +0100

    Updated a lot of things

--- src/main/java/me/waterbroodje/parkourplugin/managers/ScoreboardManager.java
+++ src/main/java/me/waterbroodje/parkourplugin/managers/ScoreboardManager.java
@@ -35,13 +35,16 @@ public class ScoreboardManager {
                         scoreboardPlayers.remove(uuid);
                         return;
                     }
+                    System.out.println("---");
+                    System.out.println(Bukkit.getPlayer(uuid) + ":" + uuid + ":" + MongoDatabase.getTimeFormatted(uuid) + ":" + MongoDatabase.getTime(uuid));
+                    System.out.println("---");
                     Scoreboard board = Bukkit.getPlayer(uuid).getScoreboard();
-                    board.getTeam("bestAttempt").setPrefix(Main.chat("&e&lBest Attempt&f: " + MongoDatabase.getTime(uuid) + "s"));
-                    board.getTeam("leaderboard5").setPrefix(Main.chat(list.get(0) == null ? "&7N/A" : list.get(0)));
-                    board.getTeam("leaderboard4").setPrefix(Main.chat(list.get(1) == null ? "&7N/A" : list.get(1)));
-                    board.getTeam("leaderboard3").setPrefix(Main.chat(list.get(2) == null ? "&7N/A" : list.get(2)));
-                    board.getTeam("leaderboard2").setPrefix(Main.chat(list.get(3) == null ? "&7N/A" : list.get(3)));
-                    board.getTeam("leaderboard1").setPrefix(Main.chat(list.get(4) == null ? "&7N/A" : list.get(4)));
+                    board.getTeam("bestAttempt").setPrefix(Main.chat("&e&lBest Attempt&f: " + MongoDatabase.getTimeFormatted(uuid)));
+                    board.getTeam("leaderboard5").setPrefix(Main.chat(list.get(0) == null ? "&e #5 &7- &enone &7- &f0s" : "&e #5 " + list.get(0)));
+                    board.getTeam("leaderboard4").setPrefix(Main.chat(list.get(1) == null ? "&e #4 &7- &enone &7- &f0s" : "&e #4 " + list.get(1)));
+                    board.getTeam("leaderboard3").setPrefix(Main.chat(list.get(2) == null ? "&e #3 &7- &enone &7- &f0s" : "&e #3 " + list.get(2)));
+                    board.getTeam("leaderboard2").setPrefix(Main.chat(list.get(3) == null ? "&e #2 &7- &enone &7- &f0s" : "&e #2 " + list.get(3)));
+                    board.getTeam("leaderboard1").setPrefix(Main.chat(list.get(4) == null ? "&e #1 &7- &enone &7- &f0s" : "&e #1 " + list.get(4)));
                 }
             }
         }.runTaskTimer(Main.getInstance(), 0L, 20 * 5);
@@ -68,27 +71,27 @@ public class ScoreboardManager {
 
         Team lb1 = board.registerNewTeam("leaderboard1");
         lb1.addEntry(ChatColor.GREEN + "");
-        lb1.setPrefix(Main.chat(list.get(4) == null ? "&7N/A" : list.get(4)));
+        lb1.setPrefix(Main.chat(list.get(4) == null ? "&e #1 &7- &enone &7- &f0s" : "&e #1 " + list.get(4)));
         obj.getScore(ChatColor.GREEN + "").setScore(5);
 
         Team lb2 = board.registerNewTeam("leaderboard2");
         lb2.addEntry(ChatColor.RED + "");
-        lb2.setPrefix(Main.chat(list.get(3) == null ? "&7N/A" : list.get(3)));
+        lb2.setPrefix(Main.chat(list.get(3) == null ? "&e #2 &7- &enone &7- &f0s" : "&e #2 " + list.get(3)));
         obj.getScore(ChatColor.RED + "").setScore(4);
 
         Team lb3 = board.registerNewTeam("leaderboard3");
         lb3.addEntry(ChatColor.BLUE + "");
-        lb3.setPrefix(Main.chat(list.get(2) == null ? "&7N/A" : list.get(2)));
+        lb3.setPrefix(Main.chat(list.get(2) == null ? "&e #3 &7- &enone &7- &f0s" : "&e #3 " + list.get(2)));
         obj.getScore(ChatColor.BLUE + "").setScore(3);
 
         Team lb4 = board.registerNewTeam("leaderboard4");
         lb4.addEntry(ChatColor.GRAY + "");
-        lb4.setPrefix(Main.chat(list.get(1) == null ? "&7N/A" : list.get(1)));
+        lb4.setPrefix(Main.chat(list.get(1) == null ? "&e #4 &7- &enone &7- &f0s" : "&e #4 " + list.get(1)));
         obj.getScore(ChatColor.GRAY + "").setScore(2);
 
         Team lb5 = board.registerNewTeam("leaderboard5");
         lb5.addEntry(ChatColor.GOLD + "");
-        lb5.setPrefix(Main.chat(list.get(0) == null ? "&7N/A" : list.get(0)));
+        lb5.setPrefix(Main.chat(list.get(0) == null ? "&e #5 &7- &enone &7- &f0s" : "&e #5 " + list.get(0)));
         obj.getScore(ChatColor.GOLD + "").setScore(1);
 
         this.scoreboard = board;
